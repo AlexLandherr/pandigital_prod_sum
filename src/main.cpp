5504f7dbb9c48f352bced11efb4203dcb4373005
Alex Landherr <alex.landherr@gmail.com>
2023-12-26 17:04:43 +0000

    Tested algorithm to check multiplicand/multiplier/product identity pandigital-ness.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "include/functions.h"
 #include <iostream>
+#include <algorithm>
+#include <string>
 
 /*
 We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once;
@@ -14,6 +16,45 @@ HINT: Some products can be obtained in more than one way so be sure to only incl
 */
 
 int main() {
+    std::string digits = "123456789";
+    std::string multiplicand = std::to_string(39);
+    std::string multiplier = std::to_string(186);
+    std::string product = std::to_string(7254);
+    bool is_1_to_n_pandigital = true;
+
+    //Checking multiplicand.
+    std::cout << "Before multiplicand: " << digits << '\n';
+    for (char c : multiplicand) {
+        if (digits.find(c) != std::string::npos) {
+            digits.erase(digits.find(c), 1);
+        } else {
+            is_1_to_n_pandigital = false;
+        }
+    }
+    std::cout << "After multiplicand: " << digits << '\n';
+
+    //Checking multiplier.
+    std::cout << "Before multiplier: " << digits << '\n';
+    for (char c : multiplier) {
+        if (digits.find(c) != std::string::npos) {
+            digits.erase(digits.find(c), 1);
+        } else {
+            is_1_to_n_pandigital = false;
+        }
+    }
+    std::cout << "After multiplier: " << digits << '\n';
+
+    //Checking product.
+    std::cout << "Before product: " << digits << '\n';
+    for (char c : product) {
+        if (digits.find(c) != std::string::npos) {
+            digits.erase(digits.find(c), 1);
+        } else {
+            is_1_to_n_pandigital = false;
+        }
+    }
+    std::cout << "After product: " << digits << '\n';
+    std::cout << "Is multiplicand/multiplier/product identity 1 to 9 pandigital?: " << std::boolalpha << is_1_to_n_pandigital << '\n';
 
     return 0;
 }
